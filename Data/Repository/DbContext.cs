03dfaacbfa05c78cfc725ba8e90df01384240f26
Gabriel Rocha Stabile <gabrielrostabile@outlook.com>
2023-04-10 13:04:18 -0300

    chore: removed coments

--- Data/Repository/DbContext.cs
+++ Data/Repository/DbContext.cs
@@ -5,20 +5,13 @@ using TodoApi.Data.Repository.Models;
 
 namespace TodoApi.Data.Repository
 {
-	public class Context : DbContext
-	{
-        // public DbSet<Category> Category { get; set; } = null!;
-        // public DbSet<ProductModel> Product { get; set; }
-        // public DbSet<ProductModel> Product { get; set; }
-        // public DbSet<ProductModel> Product { get; set; }
-        // public DbSet<ProductModel> Product { get; set; }
-
-
-        public DbSet<CategoryModel> CategoryModel {get; set; }
-        public DbSet<ProductModel> ProductModel {get; set; }
-        public DbSet<ServiceModel> ServiceModel {get; set; }
-        public DbSet<TableModel> TableModel {get; set; }
-        public DbSet<WaiterModel> WaiterModel {get; set; }
+    public class Context : DbContext
+    {
+        public DbSet<CategoryModel> CategoryModel { get; set; }
+        public DbSet<ProductModel> ProductModel { get; set; }
+        public DbSet<ServiceModel> ServiceModel { get; set; }
+        public DbSet<TableModel> TableModel { get; set; }
+        public DbSet<WaiterModel> WaiterModel { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlite("DataSource=app.db;Cache=Shared");
