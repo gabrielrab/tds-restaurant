4b80d84775f28ee4de432a13d27b1a05a532886e
Gabriel Rabelo <contato.ideialisa@gmail.com>
2023-04-11 22:01:38 -0300

    chore: continue order page

--- Pages/Orders/Index.cshtml.cs
+++ Pages/Orders/Index.cshtml.cs
@@ -1,24 +1,31 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using TodoApi.Data.Repository;
+using TodoApi.Data.Repository.Models;
 
 namespace TodoApi.Pages.Orders
 {
-    public class Index : PageModel
+public class Index : PageModel
     {
-        private readonly ILogger<Index> _logger;
+        private readonly Context context;
 
-        public Index(ILogger<Index> logger)
+        public Index(Context context)
         {
-            _logger = logger;
+            this.context = context;
         }
+        public List<CategoryModel> categories = new();
+        public List<ProductModel> products = new();
+        public List<WaiterModel> waiters = new();
+
 
         public void OnGet()
         {
+            categories = context.CategoryModel?.ToList() ?? new List<CategoryModel>();
+            products = context.ProductModel?.ToList() ?? new List<ProductModel>();
+            waiters = context.WaiterModel?.ToList() ?? new List<WaiterModel>();
         }
     }
 }
